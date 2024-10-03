750d35891e789a4f93e02485327fa5dc2037586e
raghav4444 <2023bcs065@axiscolleges.in>
2024-10-03 23:59:52 +0530

    2

--- script.js
+++ script.js
@@ -69,15 +69,23 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 let score = 20;
-const number = Math.trunc(Math.random() * 20) + 1;
+let highScore = 0;
+let number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = ' No Number !';
-  } else if (guess == number) {
+  } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number 🎉';
+    document.querySelector('.number').textContent = number;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
@@ -97,4 +105,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
